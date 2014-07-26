d778ecc5b130e028832224385617330996661537
Amelia Laundy <amelialaundy@gmail.com>
2014-07-26 15:48:15 +1200

    adding third option for index with javascript

---
+++ other_indexes/myBlogs.js
@@ -0,0 +1,264 @@
+myFunction([
+	{
+	"display": "My Blogs",
+	"url": "title"
+	},
+	{
+	"display": "Week Seven:14th July-20th July 2014",
+	"url": "week"
+	},
+	{
+	"display": "Cultural blog: </p>",
+	"url": "/blog-posts/week7_cultural"
+	},
+	{
+	"display": "Exploring Your Values</p>",
+	"url": "explanation"
+	},
+	{
+	"display": "Technical blog:",
+	"url": "/blog-posts/week7-technical.html"
+	},
+	{
+	"display": "SQL vs NoSQL",
+	"url": "explanation"
+	},
+	{
+	"display": "Week Six:7th July-13th July 2014",
+	"url": "week"
+	},
+	{
+	"display": "Cultural blog:",
+	"url": "/blog-posts/week6_cultural"
+	},
+	{
+	"display": "Stereotype Threat",
+	"url": "explanation"
+	},
+	{
+	"display": "Technical blog:",
+	"url": "/blog-posts/week6_technical.html"
+	},
+	
+	{
+	"display": "Blocks, Procs & Lambdas",
+	"url": "explanation"
+	},
+	{
+	"display": "Week Five:30th June-6th July 2014",
+	"url": "week"
+	},
+	{
+	"display": "Cultural blog:",
+	"url": "/blog-posts/week5_cultural"
+	},
+	{
+	"display": "My thoughts on pairing and feedback so far",
+	"url": "explanation"
+	},
+	{
+	"display": "Technical blog:",
+	"url": "/blog-posts/week5_technical.html"
+	},
+	{
+	"display": "Ruby Classes",
+	"url": "explanation"
+	},
+	{
+	"display": "Week Four:23rd June 2014-29th June 2014",
+	"url": "week"
+	},
+	{
+	"display": "Cultural blog:",
+	"url": "/blog-posts/week4_cultural"
+	},
+	{
+	"display": "Where is all our data going to be stored in the future?",
+	"url": "explanation"
+	},
+	{
+	"display": "Technical blog:",
+	"url": "/blog-posts/week4_technical.html"
+	},
+	{
+	"display": "Teach about Enumberable method #group_by",
+	"url": "explanation"
+	},
+	{
+	"display": "Week Three:16th June 2014-22nd June 2014",
+	"url": "week"
+	},
+	{
+	"display": "Cultural blog:",
+	"url": "/blog-posts/week3_cultural"
+	},
+	{
+	"display": "Looking at my Gregorc thinking style and growth vs. mindset",
+	"url": "explanation"
+	},
+	{
+	"display": "Technical blog:",
+	"url": "/blog-posts/week3_technical"
+	},
+	{
+	"display": "Popularity of Javascript: The good the bad and the ugly",
+	"url": "explanation"
+	},
+	{
+	"display": "Week Two: 9th June 2014-15th June 2014",
+	"url": "week"
+	},
+	{
+	"display": "My Wireframe",
+	"url": "/unit1_projects/my_wireframe"
+	},
+	{
+	"display": "Creating a wireframe for this index website",
+	"url": "explanation"
+	},
+	{
+	"display": "Pairing session pt. 1",
+	"url": "/unit1_projects/gps1.1"
+	},
+	{
+	"display": "First pairing session",
+	"url": "explanation"
+	},
+	{
+	"display": "Pairing Session pt. 2",
+	"url": "/unit1_projects/pairing_session_steve"
+	},
+	{
+	"display": "My version of the page based on our work together",
+	"url": "explanation"
+	},
+	{
+	"display": "Technical blog:",
+	"url": "/blog-posts/week2_technical"
+	},
+	{
+	"display": "CSS Positioning",
+	"url": "explanation"
+	},
+	{
+	"display": "Week One: 2nd June 2014-8th June 2014",
+	"url": "week"
+	},
+	{
+	"display": "EDA 1:first webpage",
+	"url": "/unit1_projects/eda1.html"
+	},
+	{
+	"display": "Designed to introduce yourself...I didnt't read the brief properly!",
+	"url": "explanation"
+	},
+	{
+	"display": "EDA 2:second webpage",
+	"url": "/unit1_projects/eda2"
+	},
+	{
+	"display": "Teaching something:How to make haloumi cheese yum yum!",
+	"url": "explanation"
+	},
+	{
+	"display": "My first ever blog!",
+	"url": "/blog-posts/week1_technical"
+	},
+	{
+	"display": "Considering webpage design",
+	"url": "explanation"
+	},
+	{
+	"display": "Getting into this blogging thing!",
+	"url": "/blog-posts/week1_cultural"
+	},
+	{
+	"display": "Cultural blog:first impressions of EDA",
+	"url": "explanation"
+	},
+	{
+	"display": "Wireframing",
+	"url": "/unit1_projects/wireframe_sites"
+	},
+	{
+	"display": "First time giving wireframing a go!",
+	"url": "explanation"
+	}
+]);
+
+
+// {
+// 	"display": "",
+// 	"url": ""
+// 	},
+
+// <h2>Week Seven:14th July-20th July 2014</h2>
+// 
+// 
+// 			<a href="/blog-posts/week7_cultural">Cultural blog:</a>
+// 			<p>Exploring Your Values</p>
+// 			<a href="/blog-posts/week7-technical.html">Technical blog:</a><p>SQL vs NoSQL</p>
+// 
+// 
+// 		<h2>Week Six:7th July-13th July 2014</h2>
+// 
+// 
+// 			<a href="/blog-posts/week6_cultural"><p>Cultural blog:</a>
+// 			<p>Stereotype Threat</p>
+// 			<a href="/blog-posts/week6_technical.html">Technical blog:</a><p>Blocks, Procs & Lambdas</p>
+// 
+// 
+// 		<h2>Week Five:30th June-6th July 2014</h2>
+// 
+// 
+// 			<a href="/blog-posts/week5_cultural">Cultural blog:</a>
+// 			<p>My thoughts on pairing and feedback so far</p>
+// 			<a href="/blog-posts/week5_technical.html">Technical blog:</a><p>Ruby Classes</p>
+// 
+// 
+// 		<h2>Week Four:23rd June 2014-29th June 2014</h2>
+// 
+// 
+// 			<a href="/blog-posts/week4_cultural">Cultural blog: The Future of Data Storage</a>
+// 			<p>Where is all our data going to be stored in the future?</p>
+// 			<a href="/blog-posts/week4_technical.html">Technical blog:</a><p>Technical blog: Teach about Enumberable method #group_by</p>
+// 
+// 
+// 		<h2>Week Three:16th June 2014-22nd June 2014</h2>
+// 
+// 
+// 
+// 			<a href="/blog-posts/week3_cultural">Cultural blog:Thinking Styles</a>
+// 			<p>Looking at my Gregorc thinking style and growth vs. mindset</p>
+// 			<a href="/blog-posts/week3_technical">Technical blog: The Popularity of Javascript</a>
+// 			<p>Popularity of Javascript: The good the bad and the ugly</p>
+// 
+// 
+// 		<h2>Week Two: 9th June 2014-15th June 2014</h2>
+// 
+// 
+// 
+// 			<a href="/unit1_projects/my_wireframe">My Wireframe</a>
+// 			<p>Creating a wireframe for this index website</p>
+// 			<a href="/unit1_projects/gps1.1">Pairing session pt. 1</a>
+// 			<p>First pairing session, with Steve;This is the direct result we had after coding together</p>
+// 			<a href="/unit1_projects/pairing_session_steve">Pairing Session pt. 2</a>
+// 			<p>My version of the page based on our work together</p>
+// 			<a href="/blog-posts/week2_technical">Technical Blog</a>
+// 			<p>CSS Positioning</p>
+// 
+// 
+// 		<h2>Week One: 2nd June 2014-8th June 2014</h2>
+// 
+// 
+// 
+// 			<a href="/unit1_projects/eda1.html">EDA 1:first webpage</a>
+// 			<p>Designed to introduce yourself...I didnt't read the brief properly!</p>
+// 			<a href="/unit1_projects/eda2">EDA 2:second webpage</a>
+// 			<p>Teaching something:How to make haloumi cheese yum yum!</p>
+// 			<a href="/blog-posts/week1_technical">My first ever blog!</a>
+// 			<p>Technical blog:considering webpage design<p>
+// 			<a href="/blog-posts/week1_cultural">Getting into this blogging thing!</a>
+// 			<p>Cultural blog:first impressions of EDA</p>
+// 			<a href="/unit1_projects/wireframe_sites">Wireframing<a>
+// 			<p>First time giving wireframing a go!</p>
